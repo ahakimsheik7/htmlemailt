2636bf6bfdb9f7e63098e75caf0d718c79f8969f
Abdulhakim Sheik <sheik@Abdulhakims-MacBook-Air.local>
2022-09-15 10:26:50 -0500

    template

---
+++ images/logo2_files/authoring-context-6a0bd781.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["authoring-context"],{874:function(n,o,t){var e,i;e=[t(8),t(2),t(230)],void 0===(i=function(n,o,t){return new(t.extend({_startingCreateDoc:function(){this.loadDocumentPromise=null}}))}.apply(o,e))||(n.exports=i)}}]);
+//# sourceMappingURL=authoring-context-6a0bd781.js.map
